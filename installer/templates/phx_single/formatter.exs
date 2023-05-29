aeabd146e368513baba690129fe739b20b6f0b6e
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2023-05-29 19:24:29 +0900

    remove installer

--- installer/templates/phx_single/formatter.exs
+++
@@ -1,6 +0,0 @@
-[
-  import_deps: [<%= if @ecto do %>:ecto, :ecto_sql, <% end %>:phoenix],<%= if @ecto do %>
-  subdirectories: ["priv/*/migrations"],<% end %>
-  plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"<%= if @ecto do %>, "priv/*/seeds.exs"<% end %>]
-]
--- installer/templates/phx_web/components/layouts.ex
+++
@@ -1,5 +0,0 @@
-defmodule <%= @web_namespace %>.Layouts do
-  use <%= @web_namespace %>, :html
-
-  embed_templates "layouts/*"
-end
