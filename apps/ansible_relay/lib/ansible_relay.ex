4d77925056fd500acd7c87635c309f05285f77e4
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2023-07-20 03:16:18 +0900

    finish ansible

--- apps/ansible_relay/lib/ansible_relay.ex
+++ apps/ansible_relay/lib/ansible_relay.ex
@@ -20,19 +20,39 @@ defmodule AnsibleRelay do
     |> Relay.output()
   end
   
+  def run_playbook(%Relay{common_opts: c_opts} = relay \\ Relay.new(), opts \\ []) do
+    args =
+      [c_opts, opts]
+      |> merge_opts()
+      |> process_opts()
+      |> opts_to_args()
+
+    relay
+    |> Relay.cmd("ansible-playbook", args)
+    |> Relay.output()
+  end
+  
   defp merge_opts(opts), do: 
     Enum.reduce([@default_opts | opts], &Keyword.merge(&2, &1))
 
   defp process_opts(opts) do
     Enum.reduce(opts, [], fn 
-      {:path, path}, acc -> Keyword.update(acc, :inventory, path, & "#{path}/#{&1}")
-      {:file, file}, acc -> Keyword.update(acc, :inventory, file, & "#{&1}/#{file}")
+      {:path, path}, acc -> 
+        acc
+        |> Keyword.update(:inventory, path, & "#{path}/#{&1}")
+        |> Keyword.update(:playbook, path, & "#{path}/#{&1}")
+      {:inventory, file}, acc -> Keyword.update(acc, :inventory, file, & "#{&1}/#{file}")
+      {:playbook, file}, acc -> Keyword.update(acc, :playbook, file, & "#{&1}/#{file}")
+      {:syntax_check, bool}, acc -> Keyword.put(acc, :syntax_check, bool)
     end)
   end
   
   defp opts_to_args(opts) do
     Enum.reduce(opts, [], fn 
       {:inventory, inventory}, acc -> ["-i", inventory | acc]
+      {:playbook, playbook}, acc -> [playbook | acc]
+      {:syntax_check, true}, acc -> ["--syntax-check" | acc]
+      {:syntax_check, _}, acc -> acc
     end)
   end
 end
--- apps/ansible_relay/test/ansible_relay_test.exs
+++ apps/ansible_relay/test/ansible_relay_test.exs
@@ -4,9 +4,22 @@ defmodule AnsibleRelayTest do
   @path Path.expand("./test/priv/", File.cwd!())
   @moduletag timeout: :infinity
 
+  test "relay chaining works" do
+    Relay.new(path: @path, inventory: "gcp.yaml")
+    |> AnsibleRelay.dynamic_inventory()
+    |> AnsibleRelay.run_playbook(playbook: "main.yaml")
+    |> IO.inspect(label: "ALL")
+  end
+
   test "dynamic_inventory/1" do
-    Relay.new()
-    |> AnsibleRelay.dynamic_inventory([path: @path, file: "gcp.yaml"])
-    |> IO.inspect()
+    Relay.new(path: @path, inventory: "gcp.yaml")
+    |> AnsibleRelay.dynamic_inventory()
+    |> IO.inspect(label: "DYNAMIC INVENTORY")
+  end
+  
+  test "run_playbook/1" do
+    Relay.new(path: @path, inventory: "gcp.yaml")
+    |> AnsibleRelay.run_playbook(playbook: "main.yaml")
+    |> IO.inspect(label: "RUN PLAYBOOK")
   end
 end
