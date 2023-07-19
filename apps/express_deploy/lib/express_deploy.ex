1995180c23bc5bf541cb07490f98814bf6a95a6e
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2023-07-20 04:32:53 +0900

    add evaluator to express deploy

--- apps/express_deploy/lib/express_deploy.ex
+++ apps/express_deploy/lib/express_deploy.ex
@@ -3,16 +3,40 @@ defmodule ExpressDeploy do
   Documentation for `ExpressDeploy`.
   """
 
-  @doc """
-  Hello world.
+  @resource_directory ""
+  @temp_directory ""
 
-  ## Examples
+  def run(config, fnc) do
+    params = process_config(config)
+    resource_directory = params[:resource_directory] || @resource_directory
+    temp_directory = params[:temp_directory] || @temp_directory
 
-      iex> ExpressDeploy.hello()
-      :world
+    File.rm_rf(temp_directory)
 
-  """
-  def hello do
-    :world
+    for file <- File.ls(resource_directory) do
+      destination = 
+        file
+        |> String.replace_trailing(".eex", "")
+        |> Path.expand(temp_directory)
+
+      file
+      |> Path.expand(resource_directory)
+      |> EEx.eval_file(assigns: params)
+      |> IO.inspect()
+      |> then(&File.write(destination, &1))
+    end
+
+    fnc.(temp_directory)
+  end
+
+  defp process_config(config) do
+    Enum.reduce(config, [], fn 
+      {:credential_file, file}, acc -> Keyword.update(acc, :credential_file, file, & "#{&1}/#{file}")
+      {:resource_dir, directory}, acc -> 
+        acc
+        |> Keyword.put(:resource_dir, directory)
+        |> Keyword.update(:credential_file, directory, & "#{directory}/#{&1}")
+      {key, value}, acc -> Keyword.put(acc, key, value)
+    end)
   end
 end
--- apps/express_deploy/mix.exs
+++ apps/express_deploy/mix.exs
@@ -18,7 +18,7 @@ defmodule ExpressDeploy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :eex],
       mod: {ExpressDeploy.Application, []}
     ]
   end
